03c950137318fd048c88e7cc49b51a314ad69ec1
GkIgor <igorf0660@gmail.com>
2023-05-06 15:41:44 -0300

    Problema de busca resolvido

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-const add = document.getElementById('add');
-const body = document.querySelector('body');
-const search = document.getElementById('search');
+const add = document.getElementById("add");
+const body = document.querySelector("body");
+const search = document.getElementById("search");
 
 class Despesa {
   constructor(ano, mes, dia, tipo, descricao, valor) {
-    this.ano = ano
-    this.mes = mes
-    this.dia = dia
-    this.tipo = tipo
-    this.descricao = descricao
-    this.valor = valor
+    this.ano = ano;
+    this.mes = mes;
+    this.dia = dia;
+    this.tipo = tipo;
+    this.descricao = descricao;
+    this.valor = valor;
   }
 
   validarDespesa() {
     for (let i in this) {
-      let not = ['', undefined, null];
+      let not = ["", undefined, null];
       if (not.includes(this[i])) {
-        return false
+        return false;
       }
     }
-    return true
+    return true;
   }
 }
 
 class Bd {
   constructor() {
-    let id = localStorage.getItem('id');
+    let id = localStorage.getItem("id");
     if (id === null || id === undefined || Number.isNaN(Number(id))) {
-      localStorage.setItem('id', '0');
+      localStorage.setItem("id", "0");
     }
   }
 
   getProximoId() {
-    let proximoId = parseInt(localStorage.getItem('id'));
+    let proximoId = parseInt(localStorage.getItem("id"));
     if (isNaN(proximoId)) {
-      localStorage.setItem('id', '0');
+      localStorage.setItem("id", "0");
       proximoId = 0;
     }
     return proximoId + 1;
@@ -43,64 +43,63 @@ class Bd {
   gravar(d) {
     let id = this.getProximoId();
     localStorage.setItem(id, JSON.stringify(d));
-    localStorage.setItem('id', id);
-    this.sucessoGravar(d)
+    localStorage.setItem("id", id);
+    this.sucessoGravar(d);
   }
 
   sucessoGravar(d) {
     let { modal_header, titulo, mensagem_modal, voltar } = {
-      modal_header: document.getElementById('modal-header'),
-      titulo: document.getElementById('titulo-modal'),
-      mensagem_modal: document.getElementById('mensagem-modal'),
-      voltar: document.getElementById('voltar'),
-    }
-    modal_header.classList.add('text-success');
-    modal_header.classList.remove('text-danger');
-    titulo.innerText = 'Registro inserido com sucesso!';
-    mensagem_modal.innerText = 'Despesa foi cadastrada com sucesso!'
-    voltar.classList.add('btn-success');
-    voltar.classList.remove('btn-danger');
+      modal_header: document.getElementById("modal-header"),
+      titulo: document.getElementById("titulo-modal"),
+      mensagem_modal: document.getElementById("mensagem-modal"),
+      voltar: document.getElementById("voltar"),
+    };
+    modal_header.classList.add("text-success");
+    modal_header.classList.remove("text-danger");
+    titulo.innerText = "Registro inserido com sucesso!";
+    mensagem_modal.innerText = "Despesa foi cadastrada com sucesso!";
+    voltar.classList.add("btn-success");
+    voltar.classList.remove("btn-danger");
     for (let prop in d) {
-      document.getElementById(prop.replace("_", "-")).value = '';
+      document.getElementById(prop.replace("_", "-")).value = "";
     }
-    $('#modal-aviso').modal('show');
+    $("#modal-aviso").modal("show");
   }
 
   erroGravar(d) {
     let { modal_header, titulo, mensagem_modal, voltar } = {
-      modal_header: document.getElementById('modal-header'),
-      titulo: document.getElementById('titulo-modal'),
-      mensagem_modal: document.getElementById('mensagem-modal'),
-      voltar: document.getElementById('voltar'),
-    }
-    modal_header.classList.add('text-danger');
-    modal_header.classList.remove('text-success');
-    titulo.innerText = 'Erro ao cadastrar!';
-    mensagem_modal.innerText = 'Verifique os itens e tente novamente.';
-    voltar.classList.add('btn-danger');
-    voltar.classList.remove('btn-success');
-    $('#modal-aviso').modal('show');
+      modal_header: document.getElementById("modal-header"),
+      titulo: document.getElementById("titulo-modal"),
+      mensagem_modal: document.getElementById("mensagem-modal"),
+      voltar: document.getElementById("voltar"),
+    };
+    modal_header.classList.add("text-danger");
+    modal_header.classList.remove("text-success");
+    titulo.innerText = "Erro ao cadastrar!";
+    mensagem_modal.innerText = "Verifique os itens e tente novamente.";
+    voltar.classList.add("btn-danger");
+    voltar.classList.remove("btn-success");
+    $("#modal-aviso").modal("show");
     throw new Error(`Dados inacessiveis ou não existentes \n ${d}`);
   }
 
   recuperarTodosRegistros() {
-    let id = localStorage.getItem('id');
+    let id = parseInt(localStorage.getItem("id"));
     let despesas = [];
-    for (let i = 0; i < id; i++) {
+    for (let i = 0; i <= id; i++) {
       let despesa = JSON.parse(localStorage.getItem(i));
       if (despesa === null) {
         continue;
       }
       despesas.push(despesa);
     }
-    console.log(despesas);
-    return despesas
+    return despesas;
   }
 
   remover(indice) {
-    localStorage.removeItem(indice);
-      carregaListaDespesas();
-
+    let item = JSON.parse(localStorage.getItem(indice));
+    localStorage.removeItem(item);
+    carregaListaDespesas();
   }
 
   pesquisar() {}
@@ -108,13 +107,12 @@ class Bd {
 const bd = new Bd();
 
 function cadastrarDespesa() {
-
-  let ano = document.getElementById('ano');
-  let mes = document.getElementById('mes');
-  let dia = document.getElementById('dia');
-  let tipo = document.getElementById('tipo');
-  let descricao = document.getElementById('descricao');
-  let valor = document.getElementById('valor');
+  let ano = document.getElementById("ano");
+  let mes = document.getElementById("mes");
+  let dia = document.getElementById("dia");
+  let tipo = document.getElementById("tipo");
+  let descricao = document.getElementById("descricao");
+  let valor = document.getElementById("valor");
 
   let despesa = new Despesa(
     ano.value,
@@ -122,48 +120,50 @@ function cadastrarDespesa() {
     dia.value,
     tipo.value,
     descricao.value,
-    valor.value,
+    valor.value
   );
 
   despesa.validarDespesa() ? bd.gravar(despesa) : bd.erroGravar(despesa);
 }
 
 function carregaListaDespesas() {
-  console.log('função carregada')
   let despesas = bd.recuperarTodosRegistros();
-    let lista = document.getElementById('lista-despesas');
-    let indicex = 0;
-    lista.innerHTML = '';
-    despesas.forEach((despesa) => {
-      lista.innerHTML += `<tr id="${indicex}">
-      <td>${despesa.ano}</td>
-      <td>${despesa.mes}</td>
-      <td>${despesa.dia}</td>
-      <td>${despesa.tipo}</td>
-      <td>${despesa.descricao}</td>
-      <td>${despesa.valor}</td>
-      <td><button class="btn btn-danger" onclick="removerDespesa(${indicex})">Remover</button></td>
-      </tr>`;
-    });
-}
+  let lista = document.getElementById("lista-despesas");
+  let indice = 0;
+  lista.innerHTML = "";
+  despesas.forEach((despesa) => {
+    let mes = despesa.mes;
+    let dia = despesa.dia;
+    console.log(despesa);
+    if (mes.length == 1) {
+      mes = mes.toString().padStart(2, "0");
+    }
+    if (dia.length == 1) {
+      dia = dia.toString().padStart(2, "0");
+    }
 
-function removerDespesa(indice) {
-  bd.remover(indice);
+    lista.innerHTML += `<tr id="${indice}">
+      <td>${despesa.dia.trim()}/${mes.trim()}/${despesa.ano.trim()}</td>
+      <td>${despesa.tipo.trim()}</td>
+      <td>${despesa.descricao.trim()}</td>
+      <td>${despesa.valor.trim()}</td>
+      <td><button class="btn btn-danger" onclick="bd.remover(indice)">Remover</button></td>
+      </tr>`;
+  });
 }
 
 function pesquisarDespesa() {
-  let ano = document.getElementById('ano').value;
-  let mes = document.getElementById('mes').value;
-  let dia = document.getElementById('dia').value;
-  let tipo = document.getElementById('tipo').value;
-  let descricao = document.getElementById('descricao').value;
-  let valor = document.getElementById('valor').value;
+  let ano = document.getElementById("ano").value;
+  let mes = document.getElementById("mes").value;
+  let dia = document.getElementById("dia").value;
+  let tipo = document.getElementById("tipo").value;
+  let descricao = document.getElementById("descricao").value;
+  let valor = document.getElementById("valor").value;
 
   let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor);
   bd.pesquisar(despesa);
-
 }
 
-window.addEventListener('load', carregaListaDespesas);
-add.addEventListener('click', cadastrarDespesa);
-search.addEventListener('click', pesquisarDespesa);
+window.addEventListener("load", carregaListaDespesas);
+add.addEventListener("click", cadastrarDespesa);
+search.addEventListener("click", pesquisarDespesa);
